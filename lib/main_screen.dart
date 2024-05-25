64836d19a84cd77d0176bcd0f5ec76489053dfab
Faizan Ahmad <faizanahmad2582001@gmail.com>
2024-05-25 18:35:39 +0500

    in progress

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -7,9 +7,79 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.arrow_back_ios),
-        title: Text('T-Shirt Shop'),
-        actions: [Icon(Icons.menu)],
+        leading: Container(
+          decoration: BoxDecoration(
+            color: Colors.pink.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Icon(Icons.arrow_back_ios_new_rounded),
+        ),
+        title: Center(
+          child: Text(
+            'T-Shirt Shop',
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Colors.pink.withOpacity(0.2),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Icon(Icons.menu),
+          ),
+        ],
+      ),
+      body: GridView.builder(
+        itemCount: 10,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            childAspectRatio: 1 / 1.8),
+        itemBuilder: (context, index) {
+          return Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color: Colors.green,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Icon(Icons.favorite_border_rounded),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Image.asset(
+                      'assets/images/t4.png',
+                      fit: BoxFit.cover,
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text('Shirt Name'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text('Price'),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
       ),
     );
   }
